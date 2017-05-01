18ebd6dceedcc7437130bdc29758e92939af0c83
Ryan Marvin <marvinryan@ymail.com>
2017-05-01 10:20:45 +0300

    [Chore] Make configs env variables

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,8 +1,7 @@
-from app.config import BOT_TOKEN
+import os
 
-
-API_TOKEN = BOT_TOKEN
-ERRORS_TO = 'ryan-marvin'
+API_TOKEN = "xoxp-257303730227-286126665191-302116684915-eaefd575ae1a1f066ec839b18ef8c521"
+ERRORS_TO = "#general"
 DEFAULT_REPLY = "Sorry but I didn't understand you. Type `help` for assistance"
 PLUGINS = [
     'app',
