9f7798513c9851bd1b6c1d108cf61782ce690f96
Benjamin Wacha <bmwachajr@gmail.com>
2018-02-06 10:50:54 +0300

    feat(appUtils): dump sheet items into a equipments json file - refector scripts in utils/add_sheet_items - script extracts a list from the google sheet. - filters list into device types - dumps a list of dictionaries with each devices data
    [Delivers Get all equipment from master asset tracker and dump to json file]

--- slackbot_settings.py
+++ slackbot_settings.py
@@ -1,7 +1,7 @@
 import os
 
-API_TOKEN = "xoxp-257303730227-286126665191-302116684915-eaefd575ae1a1f066ec839b18ef8c521"
-ERRORS_TO = "#general"
+API_TOKEN = os.getenv("BOT_TOKEN")
+ERRORS_TO = os.getenv('ERRORS_TO')
 DEFAULT_REPLY = "Sorry but I didn't understand you. Type `help` for assistance"
 PLUGINS = [
     'app',
